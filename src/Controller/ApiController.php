5c051f7a938dad1db7f5df7f3ca42abf56bf9c55
Dovydas Radzys <radzysdovydas@gmail.com>
2019-04-20 13:58:00 +0300

    Fetch Volunteer

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -15,14 +15,30 @@ use Symfony\Component\Serializer\SerializerInterface;
 class ApiController extends AbstractController
 {
     /**
-     * @Route("/api/volunteers", name="api", methods={"GET", "HEAD"})
+     * @Route("/api/volunteers", name="fetchAllVolunteers", methods={"GET"})
      * @param SerializerInterface $serializer
      * @return JsonResponse
      */
-    public function fetchAllVolunteers(SerializerInterface $serializer)
+    public function fetchAllVolunteers(SerializerInterface $serializer) :JsonResponse
     {
         $volunteers = $this->getDoctrine()->getRepository(Volunteer::class)->findAll();
         $serialized = $serializer->serialize($volunteers, 'json');
+        return JsonResponse::fromJsonString(
+            $serialized,
+            200,
+            ['content-type' => 'application/json']
+        );
+    }
+
+    /**
+     * @Route("api/volunteer/{id}", name="fetchSingleVolunteer", methods={"GET"}, requirements={"id"="\d+"})
+     * @param SerializerInterface $serializer
+     * @return JsonResponse
+     */
+    public function fetchSingleVolunteer(SerializerInterface $serializer, int $id = 1):JsonResponse
+    {
+        $volunteer = $this->getDoctrine()->getRepository(Volunteer::class)->find($id);
+        $serialized = $serializer->serialize($volunteer, 'json');
         return JsonResponse::fromJsonString(
             $serialized,
             200,
