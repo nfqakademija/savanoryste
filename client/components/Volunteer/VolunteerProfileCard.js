f8fa05cf8a382fa8bff836ddb84ea33f6bfcf110
Ignas Morkunas <ignas.morkunas1@gmail.com>
2019-04-24 13:04:17 +0300

    Volunteer Card profiles fix

--- client/components/Volunteer/VolunteerProfileCard.js
+++ client/components/Volunteer/VolunteerProfileCard.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
 import Badge from 'react-bootstrap/Badge';
 
 class VolunteerProfileCard extends React.Component {
