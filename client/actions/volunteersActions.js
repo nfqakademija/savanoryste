c965b4b7e2ec1111d241184f3b9eba85218555fd
Ignas Morkunas <ignas.morkunas1@gmail.com>
2019-05-04 17:13:33 +0300

    Added getVolunteer action

--- client/actions/volunteersActions.js
+++ client/actions/volunteersActions.js
@@ -1,3 +1,8 @@
 export const getVolunteers = () => ({
-  type: 'GET_VOLUNTEERS'
+  type: 'VOLUNTEERS_CALL_REQUEST'
+});
+
+export const getVolunteer = id => ({
+  type: 'VOLUNTEER_CALL_REQUEST',
+  id
 });
