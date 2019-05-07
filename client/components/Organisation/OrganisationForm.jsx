e19475418c32986c163df6197df6b98be2e8f457
Darius Rusencik <darius.rusencik@gmail.com>
2019-05-07 23:11:00 +0300

    added organisation form, checkbox autoclosing fix

--- client/components/Organisation/OrganisationForm.jsx
+++ client/components/Organisation/OrganisationForm.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import InputGroup from 'react-bootstrap/InputGroup';
 
 
 class OrganisationForm extends React.Component {
@@ -11,7 +14,36 @@ class OrganisationForm extends React.Component {
     render() {
         
       return  (
-       <h1>hello</h1>
+        <Form>
+        <Form.Group controlId="organisationName">
+          <Form.Label className="label-text">Įmonės pavadinimas</Form.Label>
+          <Form.Control type="text" name="title" required />
+        </Form.Group>
+      
+        <Form.Group controlId="organisationEmail">
+          <Form.Label className="label-text">El. paštas</Form.Label>
+          <Form.Control type="email" name="email" required />
+        </Form.Group>
+
+        <Form.Group controlId="organisationPhone">
+        <Form.Label className="label-text">Telefonas</Form.Label>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroupPrepend">+370</InputGroup.Text>
+              </InputGroup.Prepend>
+              <Form.Control
+                type="number"
+                aria-describedby="inputGroupPrepend"
+                required/>
+              </InputGroup>
+        </Form.Group>
+
+        <Form.Group controlId="organisationAddress">
+          <Form.Label className="label-text">Adresas</Form.Label>
+          <Form.Control type="text" name="address" required />
+        </Form.Group>
+        
+      </Form>
       );
 
     }
