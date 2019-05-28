4b38a0532e79d79566b655db40c295ccbb25bf3b
Ignas Morkunas <ignas.morkunas1@gmail.com>
2019-05-28 21:08:27 +0300

    Added events endpoints, sagas

--- client/endpoints.js
+++ client/endpoints.js
@@ -2,5 +2,7 @@ export const hostUrl = 'https://savanoryste.projektai.nfqakademija.lt/api';
 
 export const endpoints = {
   volunteerList: (count, start) => `${hostUrl}/volunteers/${start}/${count}`,
-  volunteer: id => `${hostUrl}/volunteer/${id}`
+  volunteer: id => `${hostUrl}/volunteer/${id}`,
+  eventList: () => `${hostUrl}/events`,
+  event: id => `${hostUrl}/event/${id}`
 };
